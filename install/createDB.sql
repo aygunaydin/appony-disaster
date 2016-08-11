5f0685db71ef69612072a29838a0977fa12c0050
Aygun <aygunaydin@gmail.com>
2016-08-11 13:57:41 +0300

    createDB sql is updated

--- install/createDB.sql
+++ install/createDB.sql
@@ -38,84 +38,147 @@ CREATE TABLE comments
 
 ####APP_LIST TABLE DATA
 
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (583274826,
+             'bip',
+             '2016-08-03 14:21:15.0',
+             'com.turkcell.bip',
+             'knox9rVichjFEfe-dYME4Xgj4iA',
+             1);
 
-<?xml version="1.0" encoding="UTF-8"?><ROWSET>
-  <ROW>
-    <column name="appid">583,274,826</column>
-    <column name="appname">bip</column>
-    <column name="app_add_date">08/03/2016 14:21:15</column>
-    <column name="android_name">com.turkcell.bip</column>
-    <column name="androidToken">PyyyQZCTxW1Yg_Ud6CNSKVUfln0</column>
-  </ROW>
-  <ROW>
-    <column name="appid">404,239,912</column>
-    <column name="appname">fizy</column>
-    <column name="app_add_date">08/03/2016 14:31:27</column>
-    <column name="android_name">com.turkcell.gncplay</column>
-    <column name="androidToken">u7vV3bnaepQmvdcp0kUiWG4EE20</column>
-  </ROW>
-  <ROW>
-    <column name="appid">665,036,334</column>
-    <column name="appname">depo</column>
-    <column name="app_add_date">08/03/2016 14:31:57</column>
-    <column name="android_name">tr.com.turkcell.akillidepo</column>
-    <column name="androidToken">8gxtQY-FKvFVt3m6qFqKufJg5SA</column>
-  </ROW>
-  <ROW>
-    <column name="appid">335,162,906</column>
-    <column name="appname">hesabim</column>
-    <column name="app_add_date">08/03/2016 14:32:23</column>
-    <column name="android_name">com.ttech.android.onlineislem</column>
-    <column name="androidToken">8gxtQY-FKvFVt3m6qFqKufJg5SA</column>
-  </ROW>
-  <ROW>
-    <column name="appid">877,589,104</column>
-    <column name="appname">akademi</column>
-    <column name="app_add_date">08/03/2016 14:32:50</column>
-    <column name="android_name">com.turkcell.akademi</column>
-    <column name="androidToken">u7vV3bnaepQmvdcp0kUiWG4EE20</column>
-  </ROW>
-  <ROW>
-    <column name="appid">1,026,830,839</column>
-    <column name="appname">RBT</column>
-    <column name="app_add_date">08/03/2016 14:33:19</column>
-    <column name="android_name">tr.com.turkcell.calarkendinlet</column>
-    <column name="androidToken">PyyyQZCTxW1Yg_Ud6CNSKVUfln0</column>
-  </ROW>
-  <ROW>
-    <column name="appid">671,494,224</column>
-    <column name="appname">platinum</column>
-    <column name="app_add_date">08/05/2016 12:30:12</column>
-    <column name="android_name">com.turkcellplatinum.main</column>
-    <column name="androidToken">u7vV3bnaepQmvdcp0kUiWG4EE20</column>
-  </ROW>
-  <ROW>
-    <column name="appid">894,318,685</column>
-    <column name="appname">gnc</column>
-    <column name="app_add_date">08/09/2016 08:04:23</column>
-    <column name="android_name">com.solidict.gnc2</column>
-    <column name="androidToken">8gxtQY-FKvFVt3m6qFqKufJg5SA</column>
-  </ROW>
-  <ROW>
-    <column name="appid">324,684,580</column>
-    <column name="appname">spotify</column>
-    <column name="app_add_date">08/10/2016 07:18:23</column>
-    <column name="android_name">com.spotify.music</column>
-    <column name="androidToken">knox9rVichjFEfe-dYME4Xgj4iA</column>
-  </ROW>
-  <ROW>
-    <column name="appid">327,630,330</column>
-    <column name="appname">dropbox</column>
-    <column name="app_add_date">08/10/2016 07:27:41</column>
-    <column name="android_name">com.dropbox.android</column>
-    <column name="androidToken">knox9rVichjFEfe-dYME4Xgj4iA</column>
-  </ROW>
-  <ROW>
-    <column name="appid">310,633,997</column>
-    <column name="appname">whatsapp</column>
-    <column name="app_add_date">08/10/2016 07:29:02</column>
-    <column name="android_name">com.whatsapp</column>
-    <column name="androidToken">knox9rVichjFEfe-dYME4Xgj4iA</column>
-  </ROW>
-</ROWSET>
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (404239912,
+             'fizy',
+             '2016-08-03 14:31:27.0',
+             'com.turkcell.gncplay',
+             'u7vV3bnaepQmvdcp0kUiWG4EE20',
+             1);
 
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (665036334,
+             'depo',
+             '2016-08-03 14:31:57.0',
+             'tr.com.turkcell.akillidepo',
+             'knox9rVichjFEfe-dYME4Xgj4iA',
+             1);
+
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (335162906,
+             'hesabim',
+             '2016-08-03 14:32:23.0',
+             'com.ttech.android.onlineislem',
+             'FsoJuxIler9lDCglbfOFfdLzY9U',
+             1);
+
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (877589104,
+             'akademi',
+             '2016-08-03 14:32:50.0',
+             'com.turkcell.akademi',
+             'u7vV3bnaepQmvdcp0kUiWG4EE20',
+             1);
+
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (1026830839,
+             'RBT',
+             '2016-08-03 14:33:19.0',
+             'tr.com.turkcell.calarkendinlet',
+             'l8pQfTcYYxcYSAOL2HZClRH0AzU',
+             1);
+
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (671494224,
+             'platinum',
+             '2016-08-05 12:30:12.0',
+             'com.turkcellplatinum.main',
+             'u7vV3bnaepQmvdcp0kUiWG4EE20',
+             1);
+
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (894318685,
+             'gnc',
+             '2016-08-09 08:04:23.0',
+             'com.solidict.gnc2',
+             'PyyyQZCTxW1Yg_Ud6CNSKVUfln0',
+             1);
+
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (324684580,
+             'spotify',
+             '2016-08-10 07:18:23.0',
+             'com.spotify.music',
+             'knox9rVichjFEfe-dYME4Xgj4iA',
+             0);
+
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (327630330,
+             'dropbox',
+             '2016-08-10 07:27:41.0',
+             'com.dropbox.android',
+             'l8pQfTcYYxcYSAOL2HZClRH0AzU',
+             0);
+
+INSERT INTO `appony.app_list`(appid,
+                           appname,
+                           app_add_date,
+                           android_name,
+                           `androidToken`,
+                           `isTurkcell`)
+     VALUES (310633997,
+             'whatsapp',
+             '2016-08-10 07:29:02.0',
+             'com.whatsapp',
+             'l8pQfTcYYxcYSAOL2HZClRH0AzU',
+             0);
+
+COMMIT;
