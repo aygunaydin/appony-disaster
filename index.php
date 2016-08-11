f827595b566e5bfdc938a733beff4b5a0736b318
Aygun <aygunaydin@gmail.com>
2016-08-11 15:21:30 +0300

    index page is updated

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require("funcs/dbFunctions.php");
 
+//echo "INFO: jenkins - github akisi test";
 echo "<div id=\"page-wrapper\">\n"; 
 echo "\n"; 
 echo "			<!-- Header -->\n"; 
